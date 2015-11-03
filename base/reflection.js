cf51f7dee15b34bf48fbb0f909f4fedaf2d189b7
xpl <rocket.mind@gmail.com>
2015-11-03 20:03:14 +0300

    missing LogOverlay restored

--- base/reflection.js
+++ base/reflection.js
@@ -10,10 +10,10 @@ _.tests.reflection = {
         $assert ($uselessPath.length > 0) },
 
     'readSource': function () {
-        _.readSource ($uselessPath + 'useless.js', function (text) {
+        _.readSource ($uselessPath + $uselessFile, function (text) {
             $assert (text.length > 0) })
 
-        _.readSourceLine ($uselessPath + 'useless.js', 0, function (line) {
+        _.readSourceLine ($uselessPath + $uselessFile, 0, function (line) {
             $assert (line.length > 0) }) },
 
     'CallStack from error': function () {
---
+++ client/LogOverlay.js
@@ -0,0 +1,48 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+------------------------------------------------------------------------
+
+Modal overlay that outputs log.js for debugging purposes
+
+------------------------------------------------------------------------
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+(function ($ /* JQUERY */) {
+
+	LogOverlay = $singleton (Component, {
+
+		$defaults: {
+			opaque: false,   // disables passing of printed log messages to default write backend (console.log) 
+			init:   false }, // deferred init
+
+		init: function () {
+
+			log.impl.writeBackend = this.write
+
+			$(document).keydown (this.$ (function (e) {
+				if (e.keyCode === 192) { // ~
+					this.toggle () } })) },
+
+		el: $memoized ($property (function () {
+			return $('<div class="useless-log-overlay" style="display: none;">').appendTo (document.body) })),
+
+		toggle: function (yes) {
+			this.el.toggle (yes) },
+
+		write: function (params) {
+			this.toggle (true)
+			if (params.config.clear) {
+				this.el.empty () }
+            this.el.append ($('<div class="ulo-line">')
+				            	.css ('color', params.color.css)
+				            	.append ($('<span class="ulo-line-text">') .text (params.indentedText  + ' '))
+				            	.append ($('<span class="ulo-line-where">').text (params.codeLocation  + ' '))
+				            	.append ($('<span class="ulo-line-trail">').text (params.trailNewlines)))
+
+            if (!this.opaque) {
+				log.impl.defaultWriteBackend.apply (log.impl, arguments) } }
+
+	})
+
+// -- end of namespace
+
+}) (jQuery);
