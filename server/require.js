8294ecb7fd7be8924f14300b8310f812e216cb30
xpl <rocket.mind@gmail.com>
2016-04-02 15:25:06 +0300

    tabs → spaces

--- server/require.js
+++ server/require.js
@@ -9,24 +9,24 @@ module.exports = RemoteRequire = $trait ({
             2. require (['esprima', 'escodegen'], function () { $assert (esprima !== undefined) })
      */
     require: function ( jsNames, then) {          jsNames = _.coerceToArray (jsNames);
-    			  var npmNames = _.coerceToArray (jsNames).map (_.camelCaseToDashes); var self = this
+                  var npmNames = _.coerceToArray (jsNames).map (_.camelCaseToDashes); var self = this
 
             _.cps.map (npmNames, function (name, i, return_) { 
                                 var module = $global[jsNames[i]]
                                 if (module) { return_ (module) } else {
-                					        	require.$ (name).catch_ (
-                					        		   function (catched) {	log.w ('Fetching ./node_modules/' + name + ' from remote repository...')
-                					                            self.                    exec ('npm install ' + name, function ( e, ___, 						stderr) {
-                						                                                                if (e) { module.exports.fatalError (stderr) }
-                						                                                                else { _.delay (function () { var module = require (name)
-                									                                                                        		  if (module) { log.ok    ('Installed',       name) }
-                									                                                                        		  else 		  { log.error ('Install failed:', name) }
-                									                                                                   			 return_ (module) }) } }) },
-                					                	function (module) { return_ (module || util.fatalError ('Init failed:', name)) } ) } },
-                               		 
+                                                require.$ (name).catch_ (
+                                                       function (catched) { log.w ('Fetching ./node_modules/' + name + ' from remote repository...')
+                                                                self.                    exec ('npm install ' + name, function ( e, ___,                        stderr) {
+                                                                                                        if (e) { module.exports.fatalError (stderr) }
+                                                                                                        else { _.delay (function () { var module = require (name)
+                                                                                                                                      if (module) { log.ok    ('Installed',       name) }
+                                                                                                                                      else        { log.error ('Install failed:', name) }
+                                                                                                                                 return_ (module) }) } }) },
+                                                        function (module) { return_ (module || util.fatalError ('Init failed:', name)) } ) } },
+                                     
 
-                     function (					 modules) {
-                    	then.apply (self, _.map (modules,
+                     function (                  modules) {
+                        then.apply (self, _.map (modules,
                                        function (module, i) { (self[jsNames[i] + 'Ready'] || _.identity) (module, jsNames[i])
-                    											                                          $global[jsNames[i]] = module
-                    													    	                                          return module })) }) } })
+                                                                                                          $global[jsNames[i]] = module
+                                                                                                                          return module })) }) } })
