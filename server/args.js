990d8330ce24de6b735bf708d6c92dae0ff18a0b
xpl <rocket.mind@gmail.com>
2016-04-02 14:14:20 +0300

    tabs → spaces

--- server/args.js
+++ server/args.js
@@ -1,29 +1,29 @@
 var util = require ('./base/util')
 
-/*	======================================================================== */
+/*  ======================================================================== */
 
 module.exports = CommandLineArguments = $trait ({
 
-	$defaults: {
-		argKeys: { /* 'example-option': 1 */ },
-		args:    { values: [],
-				   all:    [] } },
+    $defaults: {
+        argKeys: { /* 'example-option': 1 */ },
+        args:    { values: [],
+                   all:    [] } },
 
-	argsReady: $barrier (),
+    argsReady: $barrier (),
 
     beforeInit: function (then) {
-    	this.argsReady (this.args = this.parseProcessArgs (_.keys (this.argKeys)))
-    	then () },
+        this.argsReady (this.args = this.parseProcessArgs (_.keys (this.argKeys)))
+        then () },
 
     $private: {
-	    parseProcessArgs: function (proposedKeys) {
-	        var arguments     = _.rest (process.argv, 2)
-	        var incomingKeys  = _.intersection (arguments, _.map (proposedKeys, _.camelCaseToDashes))
-	        var keysCamelCase = 						   _.map (incomingKeys, _.dashesToCamelCase)
-	        return _.extend (
-	        		_.index (keysCamelCase), { keys: 			keysCamelCase,
-	        								   keysDashed: 		incomingKeys,
-	        								   all:      		arguments,
-	            							   values: 		  _.without.apply (null, [arguments].concat (incomingKeys)) }) } } })
+        parseProcessArgs: function (proposedKeys) {
+            var arguments     = _.rest (process.argv, 2)
+            var incomingKeys  = _.intersection (arguments, _.map (proposedKeys, _.camelCaseToDashes))
+            var keysCamelCase =                            _.map (incomingKeys, _.dashesToCamelCase)
+            return _.extend (
+                    _.index (keysCamelCase), { keys:            keysCamelCase,
+                                               keysDashed:      incomingKeys,
+                                               all:             arguments,
+                                               values:        _.without.apply (null, [arguments].concat (incomingKeys)) }) } } })
 
-/*	======================================================================== */
+/*  ======================================================================== */
