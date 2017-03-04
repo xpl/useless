fc56f58beb37dd3ec5d086863c1ba6b4be783278
xpl <rocket.mind@gmail.com>
2017-03-05 00:52:05 +0300

    added server/pidfile trait

---
+++ server/pidfile.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const
+    _       = require ('underscore'),
+    fs      = require ('fs'),
+    path    = require ('path'),
+    process = require ('process')
+
+module.exports = $trait ({
+
+    $defaults: {
+        pidFile: path.resolve ('./' + path.parse (process.argv[1]).name + '.pid')
+    },
+
+    beforeInit () {
+
+        if (!this.isSupervisedProcess) {
+
+            log.ok ('Writing PID to', this.pidFile)
+
+            fs.writeFileSync (this.pidFile, process.pid + '\n', { encoding: 'ascii' })
+
+            process.on ('SIGINT', () => {
+                log.e ('Removing PID', this.pidFile)
+                try { fs.unlinkSync (this.pidFile) } catch (e) {}
+                process.exit ()
+            })
+        }
+    }
+})
